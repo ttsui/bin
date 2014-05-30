288bfebd95ac933af469e64561fa81d7c90e2d43
Tony Tsui <tsui.tony@gmail.com>
2014-05-30 12:37:24 +0100

    Add production routes.

--- setup-work-vpn-routing.sh
+++ setup-work-vpn-routing.sh
@@ -1,11 +1,23 @@
 
-VPN_NETWORKS="10.107.0.0 10.108.0.0 10.109.0.0 10.118.0.0 10.208.0.0 172.16.0.0"
+VPN_NETWORKS="10.107.0.0/16 
+10.108.0.0/16 
+10.109.0.0/16
+10.118.0.0/16
+10.208.0.0/16
+172.16.0.0/16 
+212.111.36/29
+212.111.36.8/29
+212.111.36.128/27
+212.111.36.129/32 
+212.111.36.160/27
+212.111.36.187/32
+212.111.56.32/27
+212.111.56.128/27" 
 
 VPN_IP=`ifconfig ppp0 | grep inet | awk '{ print $2 }'`
 
 for NETWORK in $VPN_NETWORKS; do
-  route add -net $NETWORK netmask 255.255.0.0 gw $VPN_IP dev ppp0
-  echo 'search youdevise.com' >> /etc/resolv.conf
+  echo sudo route add -net $NETWORK gw $VPN_IP dev ppp0
 done
 
-
+echo 'search youdevise.com' >> /etc/resolv.conf
