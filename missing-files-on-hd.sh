152e330521a1355a25a1b3199e79ca1499fb7549
Tony Tsui <tsui.tony@gmail.com>
2019-06-09 09:59:45 +1000

    Check arguments and add usage

--- missing-files-on-hd.sh
+++ missing-files-on-hd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` FILES_ON_HD.tx"
+  exit 0
+fi
+
 ls-size-and-time.sh | while read LINE; do 
   grep --silent "$LINE" $1 || echo "Missing $LINE"; 
 done 
